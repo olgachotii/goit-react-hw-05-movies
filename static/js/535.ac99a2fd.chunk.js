0ae2eb581299dfa7f0856633c1afe072ff94f3f4
olgachotii <72124804+olgachotii@users.noreply.github.com>
2022-02-14 12:56:35 +0000

    Deploying to gh-pages from @ olgachotii/goit-react-hw-05-movies@f1320563fdb409595cf544f248980f10f65f7be5 🚀

---
+++ static/js/535.ac99a2fd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[535],{535:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});var r=n(152),u=n(791),c=n(910),o=n(184);function s(e){var t=e.movieId,n=(0,u.useState)(""),s=(0,r.Z)(n,2),a=s[0],i=s[1];return(0,u.useEffect)((function(){(0,c.ZV)(t).then((function(e){e.results.length>0?i(e.results[0].content):i("there is no information yet :(")}))}),[t]),(0,o.jsx)(o.Fragment,{children:(0,o.jsx)("p",{children:a})})}}}]);
+//# sourceMappingURL=535.ac99a2fd.chunk.js.map
